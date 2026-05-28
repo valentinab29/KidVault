c0f0f9dfe783cfa7f1d01904dea5a73c2d2a6673
valentinab29 <dvbolanos@unbosque.edu.co>
2026-05-28 08:09:22 -0500

    cambios class y testimonios

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    // 1. Buscamos todas las tarjetas usando la nueva clase en español
     const tarjetas = document.querySelectorAll(".seccion-recorrido .tarjeta-paso");
 
     tarjetas.forEach(tarjeta => {
         tarjeta.addEventListener("click", () => {
             
-            // 2. Buscamos cuál es la tarjeta que tiene la clase activa en este momento
             const tarjetaActiva = document.querySelector(".seccion-recorrido .tarjeta-paso.activa");
             
-            // 3. Si encontramos una tarjeta activa, le removemos la clase para apagar el azul
             if (tarjetaActiva) {
                 tarjetaActiva.classList.remove("activa");
             }
             
-            // 4. Le ponemos la clase activa a la tarjeta que el usuario acaba de presionar
             tarjeta.classList.add("activa");
         });
     });
